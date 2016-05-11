c21b5ddaa25acdbe279d7291b03b44126128a59e
Kevin McGrath <kevin.mcgrath@sungardas.com>
2016-05-11 13:42:04 -0400

    move more particles to resouce directories

--- test/resource_route_table_association.test.js
+++ test/resource_route_table_association.test.js
@@ -4,29 +4,31 @@ var CondensationTests = require("condensation-particle-tests");
 var cTests = new CondensationTests();
 
 describe("resources", function() {
-  describe("route_table_association", function() {
-    it("creates the resource with strings", function() {
-      cTests.testParticle(
-        "resource",
-        "route_table_association",
-        require("./fixtures/resource_route_table_association_output_1"),
-        {logicalId: "RouteTableAssociation", hArgs: {routeTableId: "RouteTable", subnetId: "Subnet1"}}
-      );
-    });
+  describe("subnet", function() {
+    describe("route_table_association", function() {
+      it("creates the resource with strings", function() {
+        cTests.testParticle(
+          "resource",
+          "subnet/route_table_association",
+          require("./fixtures/resource_route_table_association_output_1"),
+          {logicalId: "RouteTableAssociation", hArgs: {routeTableId: "RouteTable", subnetId: "Subnet1"}}
+        );
+      });
 
-    it("creates the resource with refs", function() {
-      cTests.testParticle(
-        "resource",
-        "route_table_association",
-        require("./fixtures/resource_route_table_association_output_2"),
-        {
-          logicalId: "RouteTableAssociation",
-          hArgs: {
-            routeTableId: '{"Ref": "RouteTable"}',
-            subnetId: '{"Ref": "Subnet1"}'
+      it("creates the resource with refs", function() {
+        cTests.testParticle(
+          "resource",
+          "subnet/route_table_association",
+          require("./fixtures/resource_route_table_association_output_2"),
+          {
+            logicalId: "RouteTableAssociation",
+            hArgs: {
+              routeTableId: '{"Ref": "RouteTable"}',
+              subnetId: '{"Ref": "Subnet1"}'
+            }
           }
-        }
-      );
+        );
+      });
     });
   });
 });
